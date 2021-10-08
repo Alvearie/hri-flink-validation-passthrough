10c7576747fa9c4a05d794d330aa55c95e1adce8
Dan Baxter <72212524+dan-baxter@users.noreply.github.com>
2021-10-08 11:19:50 -0500

    WHFHRI-693: Initial support-2.x passthrough CI. (#3)
    * WHFHRI-693: Initial support-2.x CI.
    
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>
    
    * Delete nightly tests from support-2.x branch
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * WHFHRI-693: Move workflows to correct folder.
    
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>
    
    * Run permssion on copyright check
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * WHFHRI-693: Add missing ossPassword
    
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>
    
    * WHFHRI-693: Change version to support-2.x-SNAPSHOT
    
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>
    
    * WHFHRI-693: Update Gemfile
    
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>
    
    * WHFHRI-693: Remove cos_helper from env.rb
    
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>
    
    * WHFHRI-693: Remove iam_helper
    
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>
    
    * WHFHRI-693: Use HRI_URL instead of ingress
    
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>
    
    * WHFHRI-693: Test changing namespace to main
    
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>
    
    * WHFHRI-693: Revert NAMESPACE testing change.
    
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>
    
    * WHFHRI-693: namespace back to main again.
    
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>
    
    * WHFHRI-693: Remove HRI_SERVICE_URL
    
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>
    
    * WHFHRI-693: Point back at main NAMESPACE.
    
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>
    
    * WHFHRI-693: Fix slack webhook link.
    
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>
    
    * WHFHRI-693: Fix release slack webhook link
    
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>
    
    Co-authored-by: Dan Baxter <djbaxter@us.ibm.com>
    Co-authored-by: fred <fjricci@us.ibm.com>

--- run-nightlytests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# (C) Copyright IBM Corp. 2021
-#
-# SPDX-License-Identifier: Apache-2.0
-
-echo 'Run Service Tests'
-rspec test/nightly --tag ~@broken --format documentation --format RspecJunitFormatter --out nightlytest.xml
